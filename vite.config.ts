2298d0ba8053f8c209e0f0e05f096d313ecb5d05
macacuaiaiiaa <adrianalopes1294@gmail.com>
2025-05-24 20:39:41 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
-    allowedHosts: ['942ad303-ff3d-4800-b8d3-c8c8b82b36f2-00-1uvphesq6amx1.riker.replit.dev']
+    allowedHosts: ['lapadecu-ka.onrender.com']
   }
 });
